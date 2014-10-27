29855aada60e422e88d94a3e66dd181f1fbf2564
Mocuto Oshi <xmocuto@aim.com>
2014-10-26 22:48:09 -0400

    More changes to set up the fun stuff!

--- app.js
+++ app.js
@@ -1,12 +1,32 @@
-var app = require('express')();
+var express = require('express')
+var app = express();
+var path = require('path');
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
+var coffeeMiddleware = require('coffee-middleware');
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'mocugame')))
+app.use(express.static(path.join(__dirname, 'omni')))
+
 app.get('/', function(req, res){
-  res.sendfile('index.html');
+  res.render('index');
 });
 
-io.on('connection', function(socket){
+app.use(coffeeMiddleware({
+    src: __dirname + '/public',
+    compress: true
+}));
+
+var clientsSocket = io.of('/clients');
+
+clientsSocket.on('connection', function(socket){
+  console.log("New connection")
   socket.on('chat message', function(msg){
     console.log('message: ' + msg);
     io.emit('chat message', msg);
